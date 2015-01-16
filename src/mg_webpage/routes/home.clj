173dbe94c9528c29b2bedbb76ecf7d56bca75180
Marissa Grove <m@bushi.do>
2015-01-16 11:06:50 -0800

    Initial commit of views and routes.

---
+++ src/mg_webpage/routes/home.clj
@@ -0,0 +1,18 @@
+(ns mg-webpage.routes.home
+  (:require [compojure.core :refer :all]
+            [mg-webpage.views.layout :as layout]
+            [mg-webpage.models.project :as project]
+            [mg-webpage.models.blog :as blog]
+            [mg-webpage.models.travel :as travel]))
+
+(defn home []
+  (layout/common))
+
+(defroutes home-routes
+  (GET "/" [] (home))
+  (GET "/projects" [] project/projects-page)
+  (GET "/travels" [] travel/travels-page)
+  (GET "/blog" [] blog/blog-page)
+  (GET "/travels/:name" [name] (travel/travel-page (get travel/travel-data name)))
+  ;; (GET "/blog/:name" [name] (blog/post-page (get blog/blog-data name)))
+  )
---
+++ src/mg_webpage/views/layout.clj
@@ -0,0 +1,13 @@
+(ns mg-webpage.views.layout
+  (:require [hiccup.page :refer [html5 include-css]]
+            [mg-webpage.models.common :as common]))
+
+(defn common [& body]
+  (html5
+    [:head
+     [:title "Welcome to mg-webpage"]
+     [:link {:href "/css/screen.css", :rel "stylesheet", :type "text/css"}]]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+    [:body common/navbar
+     [:img {:src "/img/jordan.jpg" :height "35%" :width "100%"}]
+     body]))
