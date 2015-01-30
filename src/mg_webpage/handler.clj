636b528974b792b7168366c650c2fa3cc5cc0d19
Marissa Grove <m@bushi.do>
2015-01-30 15:50:57 -0800

    Initial commit of handler.clj and repl.clj

---
+++ src/mg_webpage/handler.clj
@@ -0,0 +1,29 @@
+(ns mg-webpage.handler
+  (:require [compojure.core :refer [defroutes routes]]
+            [ring.middleware.resource :refer [wrap-resource]]
+            [ring.middleware.file-info :refer [wrap-file-info]]
+            [hiccup.middleware :refer [wrap-base-url]]
+            [compojure.handler :as handler]
+            [compojure.route :as route]
+            [ring.adapter.jetty :as jetty]
+            [mg-webpage.routes.home :refer [home-routes]]
+            [environ.core :refer [env]]))
+
+(defn init []
+  (println "mg-webpage is starting"))
+
+(defn destroy []
+  (println "mg-webpage is shutting down"))
+
+(defroutes app-routes
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (-> (routes home-routes app-routes)
+      (handler/site)
+      (wrap-base-url)))
+
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty #'app {:port port :join? false})))
