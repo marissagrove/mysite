416e5cdc210877276d7b7ca553c1aaf4001d02cd
Marissa Grove <m@bushi.do>
2015-03-19 15:50:57 -0700

    Changes to the travel pages

--- src/mg_webpage/models/travel.clj
+++ src/mg_webpage/models/travel.clj
@@ -16,12 +16,12 @@
           [:link {:href "/css/screen.css", :rel "stylesheet", :type "text/css"}]]
          [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
          [:body common/navbar
-          (travel-image "africa_travels.jpg" "Africa" "/travels/africa")
-          (travel-image "canada_travels.jpg" "Canada" "/travels/canada")
-          (travel-image "europe_travels.jpg" "Europe" "/travels/europe")        
-          (travel-image "uk_travels.jpg" "Great Britain" "/travels/uk")
-          (travel-image "japan_travels.jpg" "Japan" "/travels/japan")
-          (travel-image "jordan_travels.jpg" "Jordan" "/travels/jordan")]]))
+          (travel-image "africa_travels.jpg" "Africa" "/travels/Africa")
+          (travel-image "canada_travels.jpg" "Canada" "/travels/Canada")
+          (travel-image "europe_travels.jpg" "Europe" "/travels/Europe")        
+          (travel-image "uk_travels.jpg" "Great Britain" "/travels/UK")
+          (travel-image "japan_travels.jpg" "Japan" "/travels/Japan")
+          (travel-image "jordan_travels.jpg" "Jordan" "/travels/Jordan")]]))
 
 (def place-data
   {"Africa" {:title "Africa"
@@ -29,19 +29,19 @@
              :images ["africa_travels.jpg" "africa_travels.jpg"]}
    "Canada" {:title "Canada"
              :description "description"
-             :images ["canada_travels.jpg"]}
+             :images ["canada_travels.jpg" "canada1.jpg" "canada2.jpg"]}
    "Europe" {:title "Europe"
-               :description "description"
-               :images ["europe_travels.jpg"]}
+             :description "description"
+             :images ["europe_travels.jpg" "spain1.jpg" "paris1.jpg"]}
    "Great_Britain" {:title "Great Britain"
-              :description "description"
-              :images ["uk_travels.jpg"]}
+                    :description "description"
+                    :images ["uk_travels.jpg"]}
    "Japan" {:title "Japan"
-              :description "description"
-              :images ["japan_travels.jpg"]}
+            :description "description"
+            :images ["japan_travels.jpg"]}
    "Jordan" {:title "Jordan"
-              :description "description"
-              :images ["jordan_travels.jpg"]}})
+             :description "description"
+             :images ["jordan_travels.jpg" "jordan1.jpg" "jordan4.jpg" "jordan5.jpg" "jordan6.jpg" "jordan7.jpg" "jordan8.jpg" "jordan9.jpg" "jordan10.jpg" "jordan11.jpg" "jordan12.jpg" "jordan13.jpg" "jordan14.jpg"]}})
          
 
 
