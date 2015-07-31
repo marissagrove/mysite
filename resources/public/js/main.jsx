54ea9551aaffe4faae936e2699ed7abe800ea3cd
Marissa Grove <m@bushi.do>
2015-07-30 17:08:59 -0700

    Update main.jsx

--- resources/public/js/main.jsx
+++ resources/public/js/main.jsx
@@ -669,7 +669,25 @@ return (
 }
 });
 
-var blogPosts = [{title: "Site Redesign",  body: "About a year ago I put up my personal site. A place to post my projects, photos from travels, and maybe a blog. My personal taste tends to lean toward clean and simple, so when I decided to redesign my site I didn't change anything too drastically, just made it more efficient. Originally I had my contact links in line and represented by icons, for the redesign I've put them in a dropdown menu, which to me looks much cleaner. The font weight is now much thinner, however the jury is still out on whether or not it's too thin to comfortably read! The original travel page consisted of thumbnails for each country I've traveled to. Now they are grouped by region, reducing the images on the travel page from 14 to 6, and the images are now at 100% width. Each title is a link to a comprehensive image page of photos from that region. The projects page was also thumbnails with the project name underneath, which is pretty vague. Now you can find a short description of each project and which languages/libraries/framework I used to write them. My blog page didn't have much going on, just a large image that took up most of the page. It's now been replaced by a much smaller image so you can see the most recent post without having to scroll down. Nothing drastic and still a work in progress, but it now looks more organized and up-to-date!"},
+var blogPosts = [{title: "Learning React", body: "My site has been a useful place for practice, rewriting it whenever I want to t\
+ry something new. I originally wrote my site in ClojureScript, and when I wanted to practice JavaScript I rewrote it. This last t\
+ime I used React, which is simple-ish enough to set up and allows you to work it into your project at your own speed, meaning you\
+r entire code base doesn’t have to be changed at once to get something to render. (side note: I know I could be creating knew thi\
+ngs instead of rewriting the same project every time, but I find it’s a good starting point for to get an initial grasp.) Compone\
+nts allow for an organized structure. You don’t have to write the same code in three different places, meaning a smaller more rea\
+dable codebase, and who wouldn’t want that. React’s reusable components have been most useful for the blog portion of my site. So\
+me programmers don’t want to deal with all the tedious repetition building a blog can bring about, so they’d rather use Wordpress\
+ etc. These are simple to set up and gets the job done well. However I like having everything be my own, and with React I don’t h\
+ave to cringe when I look at my blog code. I store all my blog data in one variable called blogPosts and then pass it to the comp\
+onent to render it, and when I want to add more posts I just have to add another to blogPosts and the React component will render\
+ it. It is a night and day change from how repetitive and messy my previous attempts at building a blog were. Kind of a trailing \
+thought, but React got me thinking about all the innovations in programming, there’s always something new to learn and work on. I\
+’m so impressed with all the ideas that come out of this community. I meet a lot of programmers and in times past I’d feel bored \
+by this. Why couldn’t I meet people who do something else, I’m sure they’d be so much interesting. Now that is a ridiculous notio\
+n to me, this community has so much creativity swirling around, and I just wasn’t asking the right questions. I enjoy hearing abo\
+ut people’s personal projects, what they spend their free time creating is a fun window into how they think and what their intere\
+sts are. It refreshing to come across all of this originality and creativity daily, programmers don’t get enough respect for thei\
+r artistic side."},
 {title: "Site Redesign",  body: "About a year ago I put up my personal site. A place to post my projects, photos from travels, and maybe a blog. My personal taste tends to lean toward clean and simple, so when I decided to redesign my site I didn't change anything too drastically, just made it more efficient. Originally I had my contact links in line and represented by icons, for the redesign I've put them in a dropdown menu, which to me looks much cleaner. The font weight is now much thinner, however the jury is still out on whether or not it's too thin to comfortably read! The original travel page consisted of thumbnails for each country I've traveled to. Now they are grouped by region, reducing the images on the travel page from 14 to 6, and the images are now at 100% width. Each title is a link to a comprehensive image page of photos from that region. The projects page was also thumbnails with the project name underneath, which is pretty vague. Now you can find a short description of each project and which languages/libraries/framework I used to write them. My blog page didn't have much going on, just a large image that took up most of the page. It's now been replaced by a much smaller image so you can see the most recent post without having to scroll down. Nothing drastic and still a work in progress, but it now looks more organized and up-to-date!"}];
 
 var BlogTitle = React.createClass({
