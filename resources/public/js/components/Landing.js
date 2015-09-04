28b3d66f793b4ab5ac5894bc223455e130cd2dc0
Marissa Grove <m@bushi.do>
2015-09-04 01:13:25 -0700

    Created landing page component and added the route for it

---
+++ resources/public/js/components/Landing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+var LandingPage = React.createClass({
+render: function() {
+return (
+<img src="https://s3-us-west-1.amazonaws.com/mgsite-media/austria24.jpg" />
+);
+}
+});
+
+export default LandingPage;  
