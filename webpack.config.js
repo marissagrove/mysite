e14867748e4c0066f2f84b71c8d77d7621b7f031
Sean Grove <s@bushi.do>
2015-09-21 21:07:38 -0700

    Update Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,38 @@
-var webpack = require('webpack');  
-module.exports = {  
-    entry: [
-	'webpack/hot/only-dev-server',
-	'./resources/public/js/app.js'
-    ],
-    output: {
-        path: __dirname + '/build',
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
-            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    },
-    plugins: [
-      new webpack.NoErrorsPlugin()
-    ]
+var path = require('path')
+var webpack = require('webpack')
 
-};
+module.exports = {
+  cache: true
+  , debug: true
+  , devtool: 'source-map'
+  , entry: {
+    app: './resources/public/js/route.jsx'
+  }
+  , devServer: {
+    contentBase: './public'
+  }
+  , output: {
+    path: path.join(__dirname, 'resources/public/js/dist')
+    , publicPath: '/js/dist'
+    , filename: '[name].js'
+    , chunkFilename: '[chunkhash].js'
+    , sourceMapFilename: 'debugging/[file].map'
+    , hotUpdateChunkFilename: 'hot/[id].[hash].hot-update.js'
+    , hotUpdateMainFilename: 'hot/[hash].hot-update.json'
+  }
+  , module: {
+    loaders: [
+      {test: /\.js$/, exclude:[/node_modules/, /public\/components/], loader: 'babel-loader?optional=runtime'}
+      , {test: /\.jsx$/, exclude:[/node_modules/, /public\/components/], loaders: [ 'react-hot', 'babel-loader?optional=runtime']}
+      , {test: /\.css$/, loader: 'style-loader!css-loader'}
+    ]
+    , noParse: /\.min\.js/
+  }
+  , resolve: {
+    modulesDirectories: ['public/components', 'node_modules']
+    , extensions: ['', '.js', '.jsx', '.json']
+  }
+  , plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
