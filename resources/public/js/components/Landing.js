00f7f9acea9653bba863d0c6311caec5cd667aec
Marissa Grove <m@bushi.do>
2015-09-22 16:18:44 -0700

    Replaced landing page image

--- resources/public/js/components/Landing.js
+++ resources/public/js/components/Landing.js
@@ -3,11 +3,9 @@ import React from 'react';
 var LandingPage = React.createClass({
 render: function() {
   return (
-      <div>
-      Welcome to the site of Marissa Grove, Me. It's my site
-      <br />
-      <img src="https://s3-us-west-1.amazonaws.com/mgsite-media/austria24.jpg" />
-      </div>
+	  <div className="landingimage">
+	   <img src="https://s3-us-west-1.amazonaws.com/mgsite-media/jordan.jpg" />
+	  </div>
   );
 }
 });
