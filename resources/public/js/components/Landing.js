d3b945e9cad550a16110c5606779aa9c7510ba1a
Sean Grove <s@bushi.do>
2015-09-21 21:10:31 -0700

    Whitespace fix

--- resources/public/js/components/Landing.js
+++ resources/public/js/components/Landing.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 var LandingPage = React.createClass({
 render: function() {
-return (
-<img src="https://s3-us-west-1.amazonaws.com/mgsite-media/austria24.jpg" />
-);
+  return (
+      <img src="https://s3-us-west-1.amazonaws.com/mgsite-media/austria24.jpg" />
+  );
 }
 });
 
