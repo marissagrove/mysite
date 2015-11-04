3c3ad1d5d5d77c40c5929792e339bb2307024ac1
Marissa Grove <m@bushi.do>
2015-11-04 01:07:42 -0800

    Changed website title

--- src/mg_webpage/views/layout.clj
+++ src/mg_webpage/views/layout.clj
@@ -4,7 +4,7 @@
 (defn common [& body]
   (html5
     [:head
-     [:title "Welcome to mg-webpage"]
+     [:title "Marissa"]
      [:link {:href "/css/screen.css", :rel "stylesheet", :type "text/css"}]
      [:script {:src "/js/dist/app.js", :type "application/javascript" }]]
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
